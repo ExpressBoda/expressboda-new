69988b81ab0bcd3d95eee1bd4eed118413960373
ExpressBoda <info@expressboda.com>
2026-08-17 18:26:00 +0200

    Refactor i18n settings and simplify sitemap integration
    Refactor i18n routing configuration and update sitemap integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,17 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://expressboda.com', // Указываем домен для канонических ссылок и sitemap
+  site: 'https://expressboda.com',
   trailingSlash: 'always',
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'de', 'es', 'ru'],
-    routing: 'prefix-always',
+    routing: {
+      prefixDefaultLocale: false,
+    },
   },
   integrations: [
-    sitemap({
-      i18n: {
-        defaultLocale: 'en',
-        locales: {
-          en: 'en-US',
-          de: 'de-DE',
-          es: 'es-ES',
-          ru: 'ru-RU',
-        },
-      },
-    }),
+    sitemap(),
     tailwind({
       applyBaseStyles: false,
     }),
