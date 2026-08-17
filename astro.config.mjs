fb171ac0dfe05c55c8f6ad27dfdab7a22afd1e58
Aziz Rozyev Khalikov <ar@Azizs-MacBook-Air.local>
2026-08-17 20:03:18 +0200

    feat: setup decap cms folders and files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,10 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://expressboda.com',
   trailingSlash: 'always',
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'de', 'es', 'ru'],
-    routing: {
-      prefixDefaultLocale: false,
-    },
-  },
   integrations: [
-    sitemap(),
     tailwind({
       applyBaseStyles: false,
     }),
