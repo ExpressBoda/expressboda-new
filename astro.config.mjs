aea0490a52205fcb52c72a907ac16abc23c3db6d
Aziz Rozyev Khalikov <info@expressboda.com>
2026-08-21 16:00:08 +0200

    Fix site URL in astro.config.mjs for proper routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  // Вставьте ваш домен
-  site: 'https://ваш-домен.vercel.app',
+  site: 'https://expressboda-new.vercel.app',
 
   output: 'static',
   integrations: [tailwind()],
